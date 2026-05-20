bc73769fde0d880f13d9b89a63fcd67b2c380eba
balaV-17 <aiw446821@gmail.com>
2026-05-21 00:01:47 +0200

    Add GitHub Actions deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss(), basicSsl()],
+    plugins: [react(), tailwindcss(), ],
     base: '/WebAR_Construction_Validation_Tool/',
     resolve: {
       alias: {
@@ -19,7 +18,7 @@ export default defineConfig(() => {
       hmr: process.env.DISABLE_HMR !== 'true',
       // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
-     
+      host: '0.0.0.0',
     },
   };
 });
